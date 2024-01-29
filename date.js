73597c127842af56c70b3c39dc7c81937a6bf424
Nikhil Pundir <nroyalrana@gmail.com>
2024-01-29 12:15:11 +0530

    initial

---
+++ date.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+
+export function getDate(){
+
+const today = new Date();
+
+const options = {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric'
+};
+
+   return today.toLocaleDateString("en-US", options);
+
+}
+
+export function getDay (){
+
+const today = new Date();
+
+const options = {
+  weekday: 'long'
+};
+
+   return today.toLocaleDateString("en-US", options);
+
+}
